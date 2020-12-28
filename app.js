8530b5cd9d2b203b608cc5c8fdab9c5caacf3710
Junior Hlongwane <juniorWebProjects@gmail.com>
2020-12-29 01:11:01 +0200

    Untracked and Removed Cached Node Modules + hide mongo connection properly

--- app.js
+++ app.js
@@ -1,84 +1,83 @@
 //Require Modules.
-const express = require('express');
-const mongoose = require('mongoose');
-const Blog = require('./schema');
+const express = require("express");
+const mongoose = require("mongoose");
+const Blog = require("./schema");
+const dotenv = require("dotenv"); //This is what you need to access your secret api key on the .env file within the root directory
+dotenv.config(); // YOu initialize dotenv like this
 
 //Express App.
 const app = express();
 
 //Register View Engine.
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 //Setting Static Files Public.
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //Connect To MongoDB.
-const dbURI = //'Your MongoDB URI Here'
-
-mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then((result) => app.listen(3000))
-    .catch((err) => console.log(err));
+const dbURI = process.env.mongoURI // This is how you access the mongoURI from you .env file using process.env.<name_of_key>
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
 //Listen for route GET requests on individual routes
-app.get('/', (req, res) => {
-    res.redirect('/blogs')
-})
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
+});
 
-app.get('/create', (req, res) => {
-    res.render('newBlog', {title: 'Create A New Blog'})
-})
+app.get("/create", (req, res) => {
+  res.render("newBlog", { title: "Create A New Blog" });
+});
 
+//Set blogs route to retrieve BSON blog data from MongoDB and display preview blogs.
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.render("blogsList", { title: "All blogs", blogs: result });
+    })
 
-//Set blogs route to retrieve BSON blog data from MongoDB and display preview blogs. 
-app.get('/blogs', (req, res) => {
-    Blog.find()
-        .then((result) => {
-            res.render('blogsList', {title: 'All blogs', blogs: result})
-        })
-
-        .catch((err) => {
-            console.log(err);
-        })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 //Middleware to convert our values into BSON.
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 //Post request to the server to add new blogs.
-app.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body);
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
 
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs');
-        })
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs");
+    })
 
-        .catch((err) => {
-            console.log(err);
-        })
-})
-
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 //Initializing route parameters that give each blog a unique route.
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
 
-    Blog.findById(id)
-        .then(result => {
-            res.render('blog', {blog: result, title: result.title})
-        })
+  Blog.findById(id).then((result) => {
+    res.render("blog", { blog: result, title: result.title });
+  });
 });
 
 //Adding A Button That Will Allow Administrators To Delete Blogs.
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
+app.delete("/blogs/:id", (req, res) => {
+  const id = req.params.id;
 
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-            res.json({redirect: '/blogs'});
-        })
-        .catch(err => {
-            console.log(err)
-        })
-})
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/blogs" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
